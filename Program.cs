422745c81139d1d0dc44b77c987f02637b108f52
Jeremy Eudaly <eudaly_19@hotmail.comeugit>
2020-02-09 09:42:13 -0600

    completed assignment

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HealthyHearts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int maxHR;
+
+
+            while (true)
+            {
+                Console.Write("What is your age? ");
+
+                string s = Console.ReadLine();
+                
+
+                if(int.TryParse(s, out number))
+                {
+                    maxHR = 220 - number;
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("That is not a valid entry, please try again");
+                }
+            }
+
+            Console.WriteLine("Your maximum heart rate should be {0} beats per minute", maxHR);
+
+            double lowerHRZone = maxHR * .5;
+            double upperHRZone = maxHR * .85;
+            int lower = Convert.ToInt32(lowerHRZone);
+            int upper = Convert.ToInt32(upperHRZone);
+
+
+            Console.WriteLine("Your target HR Zone is {0} - {1} beats per minute", lower, upper);
+
+        }
+
+        
+
+    }
+}
